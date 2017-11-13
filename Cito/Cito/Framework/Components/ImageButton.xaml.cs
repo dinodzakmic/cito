717f202b47c0c243c295bfe2e87669aa3ec66fe6
dinodzakmic <dino.dzakmic@gmail.com>
2017-11-13 08:59:20 +0100

    OwnerProfile rework

--- Cito/Cito/Framework/Components/ImageButton.xaml.cs
+++ Cito/Cito/Framework/Components/ImageButton.xaml.cs
@@ -16,6 +16,8 @@ namespace Cito.Framework.Components
         #endregion
         #region Public properties
 
+        #region Command
+
         public ICommand Command
         {
             get { return (ICommand)GetValue(CommandProperty); }
@@ -27,31 +29,91 @@ namespace Cito.Framework.Components
             typeof(ImageButton),
             null,
             BindingMode.TwoWay);
+        
+
+        #endregion
+        #region Button
 
         public CitoButton CitoButton
         {
             get { return Button; }
             set { Button = value; }
         }
+
+        #endregion
+        #region ButtonText
+
+        public static BindableProperty ButtonTextProperty = BindableProperty.Create(
+            propertyName: nameof(ButtonText),
+            returnType: typeof(string),
+            declaringType: typeof(ImageButton),
+            defaultBindingMode: BindingMode.TwoWay,
+            defaultValue: string.Empty,
+            propertyChanged: (b, o, n) => { ((ImageButton)b).LabelText.Text = (string)n; });
         public string ButtonText
         {
-            get { return LabelText.Text; }
-            set { LabelText.Text = value; }
+            get { return (string)GetValue(ButtonTextProperty); }
+            set { SetValue(ButtonTextProperty, value); }
         }
+
+        public double ButtonTextSize
+        {
+            get { return LabelText.FontSize; }
+            set { LabelText.FontSize = value; }
+        }
+
+        public TextAlignment ButtonTextHorizontalTextAlignment
+        {
+            get { return LabelText.HorizontalTextAlignment; }
+            set
+            {
+                if (value == TextAlignment.Center)
+                {
+                    LabelText.TranslationX = 70;
+                }
+            }
+        }
+        public TextAlignment ButtonTextVerticalTextAlignment
+        {
+            get { return LabelText.VerticalTextAlignment; }
+            set { LabelText.VerticalTextAlignment = value; }
+        }
+
+        #endregion
+        #region ButtonImage
+
+        public static BindableProperty ButtonImageProperty = BindableProperty.Create(
+            propertyName: nameof(ButtonImage),
+            returnType: typeof(ImageSource),
+            declaringType: typeof(ImageButton),
+            defaultBindingMode: BindingMode.TwoWay,
+            defaultValue: null,
+            propertyChanged: (b, o, n) => { ((ImageButton)b).Image.Source = (ImageSource)n; });
 
         public ImageSource ButtonImage
         {
-            get { return Image.Source; }
-            set { Image.Source = value; }
+            get { return (string)GetValue(ButtonImageProperty); }
+            set { SetValue(ButtonImageProperty, value); }
         }
 
-
+        #endregion
+        #region ImageHeight
+        public double ImageHeight
+        {
+            get { return Image.HeightRequest; }
+            set { Image.HeightRequest = value; }
+        }
+        #endregion
+        #region Clicked events
 
         public event EventHandler Clicked
         {
             add { Button.Clicked += value; }
             remove { Button.Clicked -= value; }
         }
+
+        #endregion
+        #region Social
 
         public Social ExternalLogin { get; set; }
 
@@ -63,13 +125,12 @@ namespace Cito.Framework.Components
 
         #endregion
 
+        #endregion
+
         public ImageButton()
         {
             InitializeComponent();
         }
 
-        #region MyRegion
-
-        #endregion
     }
 }
--- Cito/Cito/ViewModels/06OwnerProfileViewModel.cs
+++ Cito/Cito/ViewModels/06OwnerProfileViewModel.cs
@@ -8,6 +8,45 @@ namespace Cito.ViewModels
 {
     public class OwnerProfileViewModel : CitoViewModelBase
     {
-        
+        #region Bindable properties
+        #region FullName
+        private string _fullName;
+
+        public string FullName
+        {
+            get { return _fullName; }
+            set { Set(ref _fullName, value); }
+        }
+        #endregion
+        #region CarsList
+        private List<string> _carsList;
+
+        public List<string> CarsList
+        {
+            get { return _carsList; }
+            set { Set(ref _carsList, value); }
+        }
+        #endregion
+        #endregion
+        #region Commands
+
+
+        #endregion
+
+        public OwnerProfileViewModel()
+        {
+            FullName = "Firstname Lastname";
+            CarsList = new List<string>()
+            {
+                "Car1",
+                "Car2"
+            };
+        }
+
+        #region Methods
+
+
+        #endregion
+
     }
 }
