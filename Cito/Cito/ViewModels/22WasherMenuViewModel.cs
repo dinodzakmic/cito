4d653738f87ca5a655042d3a123d190ece9c155d
dinodzakmic <dino.dzakmic@gmail.com>
2017-11-20 15:05:21 +0100

    WasherMenu initial

--- Cito/Cito/ViewModels/22WasherMenuViewModel.cs
+++ Cito/Cito/ViewModels/22WasherMenuViewModel.cs
@@ -1,6 +1,38 @@
-﻿namespace Cito.ViewModels
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using Cito.Views;
+using Xamarin.Forms;
+
+namespace Cito.ViewModels
 {
     public class WasherMenuViewModel : CitoViewModelBase
     {
+        #region Bindable properties
+
+        #endregion
+        #region Commands
+        public ICommand GoToHomeCommand => new Command(async () => await GoToHome());
+        
+        public ICommand GoToEarningsCommand => new Command(async () => await GoToEarnings());
+
+        public ICommand GoToSupportCommand => new Command(async () => await GoToSupport());
+
+        #endregion
+
+        #region Methods
+        private async Task GoToHome()
+        {
+            await GoToRootPage();
+        }
+
+        private async Task GoToEarnings()
+        {
+        }
+
+        private async Task GoToSupport()
+        {
+            await GoToPage(new SupportPage());
+        }
+        #endregion
     }
 }
