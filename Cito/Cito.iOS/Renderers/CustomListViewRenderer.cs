a4c884fe230190dd0186f16ba415ae341e4d4ef0
dinodzakmic <dino.dzakmic@gmail.com>
2017-11-27 09:31:46 +0100

    ListView fixes

---
+++ Cito/Cito.iOS/Renderers/CustomListViewRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Cito.iOS.Renderers;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ListView), typeof(CustomListViewRenderer))]
+namespace Cito.iOS.Renderers
+{
+    internal class CustomListViewRenderer : ListViewRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<ListView> e)
+        {
+            base.OnElementChanged(e);         
+        }
+    }
+}
