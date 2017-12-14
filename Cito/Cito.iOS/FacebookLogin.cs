b6d37dd79e8c414f881111bb8c503b03bb76d663
Milan Keravica <keravica@gmail.com>
2017-12-14 18:37:10 +0100

    iOS Facebook login fix

--- Cito/Cito.iOS/FacebookLogin.cs
+++ Cito/Cito.iOS/FacebookLogin.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Facebook.CoreKit;
 using Facebook.LoginKit;
+using Foundation;
 using UIKit;
 
 namespace Cito.iOS
@@ -10,7 +11,7 @@ namespace Cito.iOS
     public static class FacebookLogin
     {
         internal static bool IsFacebookLogin;
-        public static void HandleFacebookLoginClicked()
+		public static void HandleFacebookLoginClicked()
         {
             if (AccessToken.CurrentAccessToken != null)
             {
@@ -26,15 +27,32 @@ namespace Cito.iOS
                 }
 
                 var manager = new LoginManager();
-                manager.LogInWithReadPermissions(new string[] { "public_profile", "email" },
-                    vc, (result, error) =>
-                        {
-                            if (error == null && !result.IsCancelled)
-                            {
-                                App.Locator.CreateAccount.FullName = Facebook.CoreKit.Profile.CurrentProfile.Name;
-                                App.Locator.Prelogin.ExternalLoginCommand.Execute(null);
-                            }
-                        });
+                manager.LogInWithReadPermissions(new string[] { "public_profile", "email" }, vc, (result, error) => {
+                    if (error == null && !result.IsCancelled)
+                    {
+						if (Profile.CurrentProfile != null && Profile.CurrentProfile.Name != null) {
+							App.Locator.CreateAccount.FullName = Facebook.CoreKit.Profile.CurrentProfile.Name;
+							App.Locator.Prelogin.ExternalLoginCommand.Execute(null);
+						} else {
+							if (result.Token != null) {
+								var graphRequest = new GraphRequest("/me", null, "GET");
+								var graphConnection = new GraphRequestConnection();
+								graphConnection.AddRequest(graphRequest, (connection, fbResult, fbError) => {
+									if (fbError == null) {
+										try {
+											var res = (NSDictionary)fbResult;
+											App.Locator.CreateAccount.FullName = res["name"].ToString();
+											App.Locator.Prelogin.ExternalLoginCommand.Execute(null);
+										} catch (Exception) {
+											App.Current.MainPage.DisplayAlert("Error", "Something went wrong", "OK");
+										}
+									}
+								});
+								graphConnection.Start();
+							}
+						}
+                    }
+                });
                 
                
             }
